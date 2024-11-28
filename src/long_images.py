80edc8040c6a30da3ef4b662dbf42caf5859787e
dudcjs2779 <dudcjs2779@gmail.com>
2024-11-28 12:57:44 +0900

    remove prev files

--- src/long_images.py
+++
@@ -1,4 +0,0 @@
-from utils import data_process
-
-image_dir = "./open_clip/data/images"
-data_process.move_long_images(image_dir)
--- src/resize_images.py
+++
@@ -1,4 +0,0 @@
-from utils import data, data_process
-
-image_dir = "/home/dudcjs/MyProject/clip-tag/open_clip/data/images"
-data_process.resize_images(image_dir)
