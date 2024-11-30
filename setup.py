3cd01f1ae91c09b7f79db0d7245061d1c3de315a
dudcjs2779 <dudcjs2779@gmail.com>
2024-11-30 20:26:56 +0900

    fix: make_frames func of data.py(setup)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tag_clip",
-    version="0.1.1",
+    version="0.1.2",
     description="Code to train the clip with tag",
     author="dudcjs2779",
     author_email="dudcjs2779@gmail.com",
